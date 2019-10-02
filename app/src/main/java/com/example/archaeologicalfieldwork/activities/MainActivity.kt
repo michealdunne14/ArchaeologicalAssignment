b74eaad29bc8db579ed75a4593b6fa43cfb395ee
Micheal Dunne <michealdunne14@gmail.com>
2019-10-02 23:12:47 +0100

    Updates on adding users to recycler view
    #error where it is not adding to recycler view

--- app/src/main/java/com/example/archaeologicalfieldwork/activities/MainActivity.kt
+++ app/src/main/java/com/example/archaeologicalfieldwork/activities/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.archaeologicalfieldwork.activities
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.archaeologicalfieldwork.R
+import com.example.archaeologicalfieldwork.adapter.HillFortAdapter
+import com.example.archaeologicalfieldwork.adapter.HillFortListener
 import com.example.archaeologicalfieldwork.main.MainApp
+import com.example.archaeologicalfieldwork.models.HillFortModel
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.startActivityForResult
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(),HillFortListener {
     lateinit var app : MainApp
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +28,22 @@ class MainActivity : AppCompatActivity() {
 
         val layoutManager = LinearLayoutManager(this)
         mListRecyclerView.layoutManager = layoutManager
-//        mListRecyclerView.adapter = HillFortMemoryStore(app,this)
+        mListRecyclerView.adapter = HillFortAdapter(app.hillforts.findAll(),this)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.item_add -> startActivityForResult<AddFortActivity>(0)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onHillFortClick(hillfort: HillFortModel) {
+        startActivityForResult(intentFor<AddFortActivity>().putExtra("placemark_edit", hillfort).putExtra("editing_Placemark", true), 0)
     }
 }
