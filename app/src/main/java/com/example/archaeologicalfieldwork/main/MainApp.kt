8e88340eb709dc050434de968e5b00414101e691
micheal <michealdunne14@gmail.com>
2019-10-23 09:56:59 +0100

    Stores Hillforts in Json file and Users

--- app/src/main/java/com/example/archaeologicalfieldwork/main/MainApp.kt
+++ app/src/main/java/com/example/archaeologicalfieldwork/main/MainApp.kt
@@ -4,20 +4,22 @@ import android.app.Application
 import com.example.archaeologicalfieldwork.activities.HillFortMemoryStore
 import com.example.archaeologicalfieldwork.activities.UserMemoryStore
 import com.example.archaeologicalfieldwork.jsonstore.HillfortJsonStore
+import com.example.archaeologicalfieldwork.jsonstore.UsersJsonStore
 import com.example.archaeologicalfieldwork.models.HillFortStore
 import com.example.archaeologicalfieldwork.models.UserModel
+import com.example.archaeologicalfieldwork.models.UserStore
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 
 class MainApp : Application(), AnkoLogger {
 
     lateinit var hillforts: HillFortStore
-    lateinit var users: UserModel
+    lateinit var users: UserStore
 
     override fun onCreate() {
         super.onCreate()
         hillforts = HillfortJsonStore(applicationContext)
-        users
+        users = UsersJsonStore(applicationContext)
         info { "App has Started" }
     }
 }
